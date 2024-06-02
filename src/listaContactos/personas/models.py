710b75d6e21176dca725dc94a8a30e621d43378b
Julio Chura <jchuraaca@unsa.edu.pe>
2024-06-01 21:00:05 -0500

    Se crea la clase Persona y se agrega la app creada a settings.py

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Models):
+    nombre = models.TextField()
+    apellidos = models.TextField()
+    edad = models.TextField()
