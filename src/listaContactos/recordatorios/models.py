a4e5f6241c7c06feeb568099087ec5063e44f4b2
Julio Chura <jchuraaca@unsa.edu.pe>
2024-06-01 21:47:28 -0500

    Se hace modificaciones en la app recordatorios y se inserta un registro

--- src/listaContactos/recordatorios/models.py
+++ src/listaContactos/recordatorios/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Recordatorios(models.Model):
+    texto = models.TextField()
+    fecha = models.DateTimeField(auto_now=False, auto_now_add=False)    
