e1d7513ac96f9c6946bdd3ead2c2150bf841d01c
JulioChura <jchuraaca@unsa.edu.pe>
2024-06-04 15:51:30 -0500

    Se crea un template para la vista myHomeView de la app inicio

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def myHomeView(request):
-    return HttpResponse("<h1>Hola desde mi app inicio</h1>")
-
+    return render(request, "home.html")
 def anotherView(request):
     return HttpResponse("<h1>Se ha creado la funcion anotherView</h1>")
