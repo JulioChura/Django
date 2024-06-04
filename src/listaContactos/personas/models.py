954fb81d16ce18fbd088a8003ce4357282453c35
JulioChura <jchuraaca@unsa.edu.pe>
2024-06-04 15:12:54 -0500

    Se agrega el campo de donar y su valor por default es False

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -6,3 +6,4 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
     edad = models.IntegerField()
+    donador = models.BooleanField(default=False)
