b655a1521207f9cc6514b6eab4cde50e54e5409a
Julio Chura <jchuraaca@unsa.edu.pe>
2024-06-07 00:07:10 -0500

    Secrea la plantilla contacto y se usa varuanles dentro de los template

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -4,6 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def myHomeView(request):
-    return render(request, "home.html")
+    varriables = {
+        "name":"Julio"
+    }
+    return render(request, "home.html", varriables)
+
 def anotherView(request):
     return HttpResponse("<h1>Se ha creado la funcion anotherView</h1>")
+
+def contacto(request): 
+    return render(request, "contacto.html")
--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from inicio.views import myHomeView, anotherView
+from inicio.views import myHomeView, anotherView, contacto
 urlpatterns = [
     path('', myHomeView, name="Pagina de inicio"),
     path('another/', anotherView),
     path('admin/', admin.site.urls),  # 
+    path('contacto/', contacto)
 ]
