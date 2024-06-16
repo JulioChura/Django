7aa89794c297d78fac3537982b6111573562cb40
Julio Chura <jchuraaca@unsa.edu.pe>
2024-06-15 22:49:11 -0500

    2.-GET se llama cuando se solicita datos al servidor, en este se responde con un formulario y el post se manifiesta cuando llenamos y enviamos la informacion, sin emabargo sale vacio debido a que en form definimos el metodo get y es por eso que en la consola se obtiene la url y post sale vacio

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -24,8 +24,8 @@ def personaCreateView(request):
     print('GET :', request.GET )
     print('POST :', request.POST)
     context = {}
-    #return render(request, 'personas/personasCreate.html', context)
     return render(request, 'personas/personasCreate.html', context)
+    #return render(request, 'personas/personasCreate.html', context)
 
 def searchForHelp(request):
     return render(request, 'personas/search.html', {})
