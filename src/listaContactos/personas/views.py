95a77e9bb47e8322f78515ab77156471271a9a9b
Julio Chura <jchuraaca@unsa.edu.pe>
2024-06-22 18:31:38 -0500

    Se hace algo de hacking y en la consola se puede ver los mensajes que se generan

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -42,6 +42,12 @@ class PersonaListView(ListView):
 
 def personaAnotherCreateView(request):
     form = RawPersonaForm()
+    if request.method == "POST":
+        form = RawPersonaForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        else:
+            print(form.errors)
     context = {
         'form':  form,
     }
